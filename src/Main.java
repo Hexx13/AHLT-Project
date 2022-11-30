5a1c6fc799602e3cbb5d15d81a52c8ba7ba2d369
Hexx13 <71945474+Hexx13@users.noreply.github.com>
2022-11-30 19:55:00 +0000

    Add some debugs to main

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreeNode;
 import java.io.IOException;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
             Parser parser = new Parser();
-            parser.parseInput("A green dog");
-        //gui
+            parser.parseInput("A green dog likes people");
 
+
+//        ParserTree tree = new ParserTree();
+//        tree.addChild( new Word("A", "1", new POS("DT")));
+//        tree.addChild( new Word("green", "1", new POS("JJ")));
+//
+//        System.out.println(((Word)(tree.getChild(1))).getWord());
     }
 }
